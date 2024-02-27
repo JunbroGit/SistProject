ecaaf2e21262063b3573d3569bb46231c3967be6
junbbangz <goo2913@naver.com>
2024-02-27 17:13:58 +0900

    day0227

--- jswork/vuework/vue-third/src/App.vue
+++ jswork/vuework/vue-third/src/App.vue
@@ -2,11 +2,11 @@
 <div>
   <div class="menu">
       <!-- <a v-for="header in 3" :key="">Home</a> -->
-      <a v-for="(menu,i) in menus" :key="i">{{ menu }}</a>
+      <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
 
-  <div class="celeb" v-for="agency in agencys" :key="agency">
-    <img src=":src='agency.imgSrc'" class="img">
+  <div class="celeb" v-for="(agency,key) in agencys" :key="key">
+    <img :src="agency.imgSrc">
       <h3>{{ agency.name }}</h3>
       <p>{{ agency.company }}</p>
       <button @click="onClickLikes(key)">좋아유</button>
@@ -23,11 +23,11 @@ export default {
       return {
 
           menus:['Home','Celeb','About'],
-          agencys:[{name:'여진구',company:'하이브',like:0,imgSrc:require('./assets/images/1.jpg')},
-          {name:'최지우',company:'SM',like:0,imgSrc:require('./assets/images/5.jpg')},
-          {name:'박보영',company:'하이브',like:0,imgSrc:require('./assets/images/12.jpg')},
-          {name:'박신혜',company:'JYP',like:0,imgSrc:require('./assets/images/13.jpg')},
-          {name:'유아인',company:'YG',like:0,imgSrc:require('./assets/images/20.jpg')}
+          agencys:[{name:'여진구',company:'하이브',likes:0,imgSrc:require('./assets/images/1.jpg')},
+          {name:'최지우',company:'SM',likes:0,imgSrc:require('./assets/images/5.jpg')},
+          {name:'박보영',company:'하이브',likes:0,imgSrc:require('./assets/images/12.jpg')},
+          {name:'박신혜',company:'JYP',likes:0,imgSrc:require('./assets/images/13.jpg')},
+          {name:'유아인',company:'YG',likes:0,imgSrc:require('./assets/images/20.jpg')}
         ],
       }
   },
@@ -52,7 +52,7 @@ export default {
       padding: 10px;
   }
 
-  .img{
+  .celeb img{
       width: 200px;
       border-radius: 30px;
   }
