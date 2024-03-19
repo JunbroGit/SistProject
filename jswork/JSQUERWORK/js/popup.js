1e65a9f2c831fdb5aeaa8917cdddb240cc53ec58
junbbangz <goo2913@naver.com>
2024-03-19 11:19:42 +0900

    day0319

--- jswork/JSQUERWORK/js/popup.js
+++
@@ -1,9 +0,0 @@
-function pop1(){
-    window.open('../image/허허/토토로1.gif','event','width=500,height=500');
-}
-
-function pop2(){
-    newpopup=window.open('../image/허허/토토로2.gif','','width=500,height=500');
-
-    setTimeout('newpopup.close()',3000);
-}
