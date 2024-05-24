7c5d10c9436f1fec2efb9ab89144e55ffb790a13
junbbangz <goo2913@naver.com>
2024-05-24 12:44:14 +0900

    day0524

--- sts4work/algorithm/src/main/java/al/Main.java
+++ sts4work/algorithm/src/main/java/al/Main.java
@@ -1,11 +1,23 @@
 package al;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World!");
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+
 	}
 
 }
